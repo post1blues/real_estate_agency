6cf18995ef83e933b35b81dc36a8a2d57d6574db
post1blues <yura1kovalyov@gmail.com>
2021-09-21 16:45:04 +0300

    Create Owner model, make data migrations

--- property/admin.py
+++ property/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address')
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
+    search_fields = ('full_name', 'owners_phonenumber', 'owner_pure_phone')
 
 
 class ComplaintAdmin(admin.ModelAdmin):
@@ -17,4 +22,5 @@ class ComplaintAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Owner, OwnerAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
--- property/models.py
+++ property/models.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -53,6 +57,16 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Owner(models.Model):
+    full_name = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+    flats = models.ManyToManyField(Flat, verbose_name='Квартиры в собственности', related_name='owners')
+
+    def __str__(self):
+        return self.full_name
 
 
 class Complaint(models.Model):
