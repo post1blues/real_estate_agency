d36fe71d124bd24ef02e4cc3e3d7fc18a4b40241
post1blues <yura1kovalyov@gmail.com>
2021-09-27 23:35:39 +0300

    Add m2m Owner Flat

--- property/admin.py
+++ property/admin.py
@@ -3,13 +3,19 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('owner', 'town', 'address')
+    search_fields = ('town', 'address')
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
+    inlines = (OwnerInline,)
 
 
 class OwnerAdmin(admin.ModelAdmin):
