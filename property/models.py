26019b4b495193866e84242e7bd0d281e7db1237
post1blues <yura1kovalyov@gmail.com>
2021-10-04 16:40:13 +0300

    Make m2m fields blankable

--- property/models.py
+++ property/models.py
@@ -48,7 +48,7 @@ class Flat(models.Model):
         db_index=True)
 
     new_building = models.NullBooleanField('Новостройка', db_index=True)
-    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул', related_name='liked_flats')
+    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул', related_name='liked_flats', blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
@@ -58,7 +58,7 @@ class Owner(models.Model):
     full_name = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
-    flats = models.ManyToManyField(Flat, verbose_name='Квартиры в собственности', related_name='owners')
+    flats = models.ManyToManyField(Flat, verbose_name='Квартиры в собственности', related_name='owners', blank=True)
 
     def __str__(self):
         return self.full_name
